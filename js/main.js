26d200138006030aa1aac5e9cb3229d03c3d4521
Laila Seif <lailaseif710@gmail.com>
2026-03-23 22:53:57 -0400

    basic files + my in class build
    i tried this assignment and my files were not pushing? it would push then not direct me to the github browser. so im restarting and hoping when i push up my starting file from class plus the usual folders/files we add for assignments will actually push up and not stash away
    
    i have my two pages i want to do. home oage for the counters then a doc page which is needed
    
    i then added in class files from the build we did together
    
    set up sass and js files based on what i saw in the requirements. step counter, inactive buttons for when i use toggle in js, etc
    
    it does seem like a big commit but they are all mostly empty, except for the assigment details so i can always refer to them :)

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { Counter } from './counter.js';
+
+const counter1 = new Counter('#counter-container1', 10);
+const counter2 = new Counter('#counter-container2', 5);
+const counter3 = new Counter('#counterCon');
+
+//const step10 - new SuperCounter(id, startingValue, step); 
