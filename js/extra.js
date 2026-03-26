272e3911b45a3db54bb55c1c6b5c1735ecf6eda7
Laila Seif <lailaseif710@gmail.com>
2026-03-25 22:44:14 -0400

    i did mobile nav with toggle and close button (did of redoing)
    i mainly worked on styling the header/nav so the mobile menu can slide in better and behave more responsively. i also adjusted the counter sizing a bit and made sure those style changes carried through into my compiled css too
    
    before i just didnt like how basic the menu was, so i did my usual overlay menu

--- js/extra.js
+++ js/extra.js
@@ -10,8 +10,15 @@ startButton.addEventListener('click', hideStartSection);
 
 //menu toggle
 const menuToggle = document.querySelector('#menu-toggle');
+const headerElement = document.querySelector('header');
 const nav = document.querySelector('nav');
+const closeButton = document.querySelector('.close-menu');
 
 menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('hidden');
+    headerElement.classList.toggle('nav-open');
 });
+
+function menuClose() {
+    headerElement.classList.remove('nav-open');
+}
+closeButton.addEventListener('click', menuClose);
